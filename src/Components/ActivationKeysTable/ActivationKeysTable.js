350aae63c6d8b64a01fb27e3abb3dac2bc84bea2
blower <blower@redhat.com>
2024-10-18 15:28:21 -0400

    "Added Updated Date Value to AK Table with sort, updated snapshot"

--- src/Components/ActivationKeysTable/ActivationKeysTable.js
+++ src/Components/ActivationKeysTable/ActivationKeysTable.js
@@ -14,9 +14,62 @@ const ActivationKeysTable = (props) => {
     role: 'Role',
     serviceLevel: 'SLA',
     usage: 'Usage',
+    updatedAt: 'Updated Date',
   };
   const { isLoading, error, data } = useActivationKeys();
+  console.log(data);
+  const [activeSortIndex, setActiveSortIndex] = React.useState(null);
+  const [sortedData, setSortedData] = React.useState([]);
+  const [activeSortDirection, setActiveSortDirection] = React.useState(null);
   const location = useLocation();
+  React.useEffect(() => {
+    if (data) {
+      setSortedData(data);
+    }
+  }, [data]);
+
+  const getSortParams = (columnIndex) => ({
+    sortBy: {
+      index: activeSortIndex,
+      direction: activeSortDirection,
+      defaultDirection: 'asc',
+    },
+    onSort: (_event, index, direction) => {
+      setActiveSortIndex(index);
+      setActiveSortDirection(direction);
+      const sorted = [...sortedData].sort((a, b) => {
+        const aValue = a[columnNames[index]];
+        const bValue = b[columnNames[index]];
+        if (index === 4) {
+          const aDate = new Date(a.updatedAt);
+          const bDate = new Date(b.updatedAt);
+          if (direction === 'asc') {
+            return aDate - bDate;
+          } else {
+            return bDate - aDate;
+          }
+        }
+        if (aValue < bValue) return direction === 'asc' ? -1 : 1;
+        if (aValue > bValue) return direction === 'asc' ? 1 : -1;
+        return 0;
+      });
+      setSortedData(sorted);
+    },
+    columnIndex,
+  });
+
+  const printDate = (dateString) => {
+    const date = new Date(dateString);
+    if (isNaN(date)) {
+      return 'Invalid Date';
+    }
+    const month = date.getUTCMonth() + 1;
+    const day = date.getUTCDate();
+
+    return `${date.getUTCFullYear()}-${month < 10 ? `0${month}` : month}-${
+      day < 10 ? `0${day}` : day
+    }`;
+  };
 
   const Results = () => {
     return (
@@ -27,11 +80,14 @@ const ActivationKeysTable = (props) => {
             <Th>{columnNames.role}</Th>
             <Th>{columnNames.serviceLevel}</Th>
             <Th>{columnNames.usage}</Th>
+            <Th sort={getSortParams(4)} width={20}>
+              {columnNames.updatedAt}
+            </Th>
             <Td></Td>
           </Tr>
         </Thead>
         <Tbody>
-          {data.map((datum) => {
+          {sortedData.map((datum) => {
             return (
               <Tr key={datum.name} ouiaSafe={true}>
                 <Td modifier="breakWord" dataLabel={columnNames.name}>
@@ -45,6 +101,11 @@ const ActivationKeysTable = (props) => {
                   {datum.serviceLevel}
                 </Td>
                 <Td dataLabel={columnNames.usage}>{datum.usage}</Td>
+                <Td dataLabel={columnNames.updatedAt}>
+                  {datum.updatedAt == new Date(0)
+                    ? 'Not Available'
+                    : printDate(datum.updatedAt)}
+                </Td>
                 <Td>
                   <DeleteActivationKeyButton
                     onClick={() => onDelete(datum.name)}
