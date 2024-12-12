235a3b7f9ba0419c96fd307992994b5e82325e8a
blower <blower@redhat.com>
2024-12-12 09:39:22 -0500

    This change allows white spaces as inputs but only a whitespace as input is invalid.

--- src/Components/Modals/CreateActivationKeyWizard.js
+++ src/Components/Modals/CreateActivationKeyWizard.js
@@ -36,8 +36,7 @@ const nameValidator = (newName, keyNames) => {
 
   return match.length == 0 && nameRegex.test(newName);
 };
-
-const descriptionValidator = /^[^\s]{1,255}$/;
+const descriptionValidator = /^(?!\s*$).{1,255}$/;
 
 const CreateActivationKeyWizard = ({
   handleModalToggle,
