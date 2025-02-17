772f60ba8a09e6c140327d18dfdedec159aa4918
(Jonathan) Luke Green <me@lukegreen.xyz>
2025-02-17 12:44:43 -0600

    TUSC-140: Add rpm type filter to Add Additional Repositories modal (#79)
    Add rpm type filter to Add Additional Repositories modal
    
    This adds the ability to filter by rpm type when looking at available
    repositories. It also changes this table from single-select filtering to
    multi-select so that users can search by multiple different parameters
    at once.
    
    User-friendly groupings for rpm types are defined per the AC in the
    ticket:
    * source => source, source_iso
    * debug => debug
    * binary => binary, binary_image, binary_iso

--- src/Components/AddAdditionalRepositoriesTable/AddAdditionalRepositoriesTable.js
+++ src/Components/AddAdditionalRepositoriesTable/AddAdditionalRepositoriesTable.js
@@ -29,9 +29,10 @@ const AddAdditionalRepositoriesTable = (props) => {
 
   const sort_by_index = ['repo_name', 'repo_label'];
 
-  const [filter, setFilter] = useDebouncedState('', 300);
+  const [repoNameFilter, setRepoNameFilter] = useDebouncedState('', 300);
+  const [repoLabelFilter, setRepoLabelFilter] = useDebouncedState('', 300);
+  const [rpmTypeFilter, setRpmTypeFilter] = useState([]);
   const queryClient = useQueryClient();
-  const [filterBy, setFilterBy] = useState('repo_name');
   const [onlyShowSelectedRepositories, setOnlyShowSelectedRepositories] =
     useState(false);
   const [page, setPage] = useState(1);
@@ -42,19 +43,59 @@ const AddAdditionalRepositoriesTable = (props) => {
   const friendlyNameMap = {
     repo_name: 'Name',
     repo_label: 'Label',
+    rpm_type: 'RPM Type',
+  };
+
+  const filters = {
+    repo_name: { value: repoNameFilter, set: setRepoNameFilter },
+    repo_label: { value: repoLabelFilter, set: setRepoLabelFilter },
+    rpm_type: {
+      value: rpmTypeFilter,
+      set: setRpmTypeFilter,
+      opts: ['binary', 'debug', 'source'],
+      placeholder: 'Type',
+    },
   };
 
   const attrMap = {
     repo_name: 'repositoryName',
     repo_label: 'repositoryLabel',
+    rpm_type: 'rpmType',
+  };
+
+  const matchFilters = (repository) => {
+    for (const [k, filter] of Object.entries(filters)) {
+      if (!Array.isArray(filter.value)) {
+        if (
+          !repository[attrMap[k]]
+            .toLowerCase()
+            .includes(filter.value.toLowerCase())
+        ) {
+          return false;
+        }
+      } else {
+        if (
+          filter.value.length > 0 &&
+          filter.value.filter((option) =>
+            repository[attrMap[k]].toLowerCase().includes(option.toLowerCase())
+          ).length == 0
+        ) {
+          return false;
+        }
+      }
+    }
+    return true;
   };
 
   const { data: repositoriesData, isLoading } = useAvailableRepositories(
     keyName,
     page,
     perPage,
-    filterBy,
-    filter,
+    {
+      repo_name: repoNameFilter,
+      repo_label: repoLabelFilter,
+      rpm_type: rpmTypeFilter,
+    },
     activeSortBy,
     activeSortDirection
   );
@@ -63,7 +104,7 @@ const AddAdditionalRepositoriesTable = (props) => {
 
   useEffect(() => {
     setPage(1);
-  }, [onlyShowSelectedRepositories, filter]);
+  }, [onlyShowSelectedRepositories, JSON.stringify(filters)]);
 
   useEffect(() => {
     prefetchNextPage(
@@ -71,12 +112,15 @@ const AddAdditionalRepositoriesTable = (props) => {
       keyName,
       page + 1,
       perPage,
-      filterBy,
-      filter,
+      {
+        repo_name: repoNameFilter,
+        repo_label: repoLabelFilter,
+        rpm_type: rpmTypeFilter,
+      },
       activeSortBy,
       activeSortDirection
     );
-  }, [page, perPage, filter]);
+  }, [page, perPage, JSON.stringify(filters)]);
 
   const getSortParams = (columnIndex) => ({
     sortBy: {
@@ -112,15 +156,7 @@ const AddAdditionalRepositoriesTable = (props) => {
   );
 
   const displayedSelectedRepos = selectedRepositories
-    .filter((repo) => {
-      if (filter == '') {
-        return true;
-      } else {
-        return repo[attrMap[filterBy]]
-          .toLowerCase()
-          .includes(filter.toLowerCase());
-      }
-    })
+    .filter((repo) => matchFilters(repo))
     .filter((_, i) => {
       return i >= (page - 1) * perPage && i < page * perPage;
     })
@@ -148,7 +184,18 @@ const AddAdditionalRepositoriesTable = (props) => {
       </EmptyStateBody>
       <EmptyStateFooter>
         <EmptyStateActions>
-          <Button variant="link" onClick={() => setFilter('')}>
+          <Button
+            variant="link"
+            onClick={() => {
+              Object.values(filters).forEach((filter) => {
+                if (Array.isArray(filter.value)) {
+                  filter.set([]);
+                } else {
+                  filter.set('');
+                }
+              });
+            }}
+          >
             Clear all filters
           </Button>
         </EmptyStateActions>
@@ -160,10 +207,7 @@ const AddAdditionalRepositoriesTable = (props) => {
     <>
       <AddAdditionalRepositoriesToolbar
         friendlyNameMap={friendlyNameMap}
-        filter={filter}
-        setFilter={setFilter}
-        filterBy={filterBy}
-        setFilterBy={setFilterBy}
+        filters={filters}
         dropdownSelectisDisabled={isSubmitting}
         selectedOnlyToggleIsDisabled={
           (!onlyShowSelectedRepositories &&
@@ -179,8 +223,8 @@ const AddAdditionalRepositoriesTable = (props) => {
         <Thead>
           <Tr>
             <Th />
-            <Th sort={getSortParams(0)}>{friendlyNameMap.repositoryName}</Th>
-            <Th sort={getSortParams(1)}>{friendlyNameMap.repositoryLabel}</Th>
+            <Th sort={getSortParams(0)}>{friendlyNameMap.repo_name}</Th>
+            <Th sort={getSortParams(1)}>{friendlyNameMap.repo_label}</Th>
           </Tr>
         </Thead>
         <Tbody>
--- src/Components/AddAdditionalRepositoriesTable/AddAdditionalRepositoriesToolbar.js
+++ src/Components/AddAdditionalRepositoriesTable/AddAdditionalRepositoriesToolbar.js
@@ -7,16 +7,14 @@ import { ToolbarGroup } from '@patternfly/react-core/dist/dynamic/components/Too
 import { ToolbarItem } from '@patternfly/react-core/dist/dynamic/components/Toolbar';
 import { ToggleGroup } from '@patternfly/react-core/dist/dynamic/components/ToggleGroup';
 import { Select, SelectOption } from '@patternfly/react-core/deprecated';
-
 import FilterIcon from '@patternfly/react-icons/dist/dynamic/icons/filter-icon';
 import propTypes from 'prop-types';
+import { Chip } from '@patternfly/react-core/dist/dynamic/components/Chip';
+import { ChipGroup } from '@patternfly/react-core/dist/dynamic/components/Chip';
 
 const AddAdditionalRepositoriesToolbar = ({
   friendlyNameMap,
-  filter,
-  setFilter,
-  filterBy,
-  setFilterBy,
+  filters,
   selectedOnlyToggleIsDisabled,
   searchIsDisabled,
   dropdownSelectisDisabled,
@@ -26,6 +24,11 @@ const AddAdditionalRepositoriesToolbar = ({
 }) => {
   const [isSelectFilterByExpanded, setIsSelectFilterByExpanded] =
     useState(false);
+
+  const [isMultiSelectOptionsExanded, setIsMultiSelectOptionsExpanded] =
+    useState(false);
+
+  const [activeFilter, setActiveFilter] = useState(Object.keys(filters)[0]);
 
   return (
     <Toolbar id="add-additional-repositories-toolbar">
@@ -38,30 +41,61 @@ const AddAdditionalRepositoriesToolbar = ({
                 setIsSelectFilterByExpanded(isSelectFilterByExpanded)
               }
               toggleIcon={<FilterIcon />}
-              placeholderText={friendlyNameMap[filterBy]}
+              placeholderText={friendlyNameMap[activeFilter]}
               onSelect={(_, value) => {
-                setFilterBy(value);
+                setActiveFilter(value[0]);
                 setIsSelectFilterByExpanded(false);
               }}
               isDisabled={dropdownSelectisDisabled}
             >
-              <SelectOption value="repo_name">
-                {friendlyNameMap.repo_name}
-              </SelectOption>
-              <SelectOption value="repo_label">
-                {friendlyNameMap.repo_label}
-              </SelectOption>
+              {Object.entries(filters).map(([k, v]) => {
+                return (
+                  <SelectOption value={[k, v]} key={k}>
+                    {friendlyNameMap[k]}
+                  </SelectOption>
+                );
+              })}
             </Select>
           </ToolbarItem>
           <ToolbarItem>
-            <SearchInput
-              placeholder={`Filter by ${friendlyNameMap[filterBy]}`}
-              value={filter}
-              onChange={(_event, value) => setFilter(value)}
-              isDisabled={searchIsDisabled}
-              onClear={() => setFilter('')}
-              style={{ width: '400px' }}
-            />
+            {!Array.isArray(filters[activeFilter].value) && (
+              <SearchInput
+                placeholder={`Filter by ${friendlyNameMap[activeFilter]}`}
+                value={filters[activeFilter].value}
+                onChange={(_event, value) => filters[activeFilter].set(value)}
+                isDisabled={searchIsDisabled}
+                onClear={() => filters[activeFilter].set('')}
+                style={{ width: '400px' }}
+              />
+            )}
+            {Array.isArray(filters[activeFilter].value) && (
+              <Select
+                isOpen={isMultiSelectOptionsExanded}
+                placeholderText={filters[activeFilter].placeholder}
+                onToggle={(_, isOpen) =>
+                  setIsMultiSelectOptionsExpanded(isOpen)
+                }
+                onSelect={(_, value) => {
+                  filters[activeFilter].set([
+                    ...filters[activeFilter].value,
+                    value,
+                  ]);
+                  setIsMultiSelectOptionsExpanded(false);
+                }}
+              >
+                {filters[activeFilter].opts.map((opt) => {
+                  return (
+                    <SelectOption
+                      key={opt}
+                      isDisabled={filters[activeFilter].value.includes(opt)}
+                      value={opt}
+                    >
+                      {opt}
+                    </SelectOption>
+                  );
+                })}
+              </Select>
+            )}
           </ToolbarItem>
           <ToolbarItem>
             <ToggleGroup>
@@ -91,16 +125,44 @@ const AddAdditionalRepositoriesToolbar = ({
           {pagination}
         </ToolbarItem>
       </ToolbarContent>
+      <ToolbarContent>
+        <ToolbarGroup>
+          {Object.entries(filters)
+            .filter(([, v]) =>
+              Array.isArray(v.value) ? v.value.length > 0 : v.value !== ''
+            )
+            .map(([k, v]) => (
+              <ToolbarItem key={k}>
+                <ChipGroup categoryName={friendlyNameMap[k]}>
+                  {Array.isArray(v.value) && (
+                    <>
+                      {v.value.map((filter, i) => (
+                        <Chip
+                          key={i}
+                          onClick={() => {
+                            v.set(v.value.toSpliced(i, 1));
+                          }}
+                        >
+                          {filter}
+                        </Chip>
+                      ))}
+                    </>
+                  )}
+                  {!Array.isArray(v.value) && (
+                    <Chip onClick={() => v.set('')}>{v.value}</Chip>
+                  )}
+                </ChipGroup>
+              </ToolbarItem>
+            ))}
+        </ToolbarGroup>
+      </ToolbarContent>
     </Toolbar>
   );
 };
 
 AddAdditionalRepositoriesToolbar.propTypes = {
   friendlyNameMap: propTypes.object.isRequired,
-  filter: propTypes.string.isRequired,
-  setFilter: propTypes.func.isRequired,
-  filterBy: propTypes.string.isRequired,
-  setFilterBy: propTypes.func.isRequired,
+  filters: propTypes.object.isRequired,
   selectedOnlyToggleIsDisabled: propTypes.bool.isRequired,
   searchIsDisabled: propTypes.bool.isRequired,
   pagination: propTypes.object.isRequired,
