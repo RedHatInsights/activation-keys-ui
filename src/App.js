b0521016b3af4b5b0ae90a80e7141905e6007ace
Luke Green <lgreen@redhat.com>
2025-12-08 11:45:23 -0600

    fix notification errors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import AppRoutes from './Routes';
-import NotificationsPortal from '@redhat-cloud-services/frontend-components-notifications/NotificationPortal';
-import { notificationsReducer } from '@redhat-cloud-services/frontend-components-notifications/redux';
-import { RegistryContext } from './store';
 import NotificationProvider from './contexts/NotificationProvider';
 import Notifications from './Components/Notifications';
 
 const App = () => {
-  const { getRegistry } = useContext(RegistryContext);
-  useEffect(() => {
-    getRegistry().register({ notifications: notificationsReducer });
-  }, [getRegistry]);
-
   return (
     <>
-      <NotificationsPortal />
       <NotificationProvider>
         <Notifications />
         <AppRoutes />
